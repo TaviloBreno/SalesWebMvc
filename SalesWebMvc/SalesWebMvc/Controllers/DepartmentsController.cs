66b5035fe35d2f4eaed4e7eae6d1006cd67e66e2
Tavilo Breno <breno_wk2@hotmail.com>
2023-05-18 17:17:28 -0300

    Criação do primeiro model-view - Department

---
+++ SalesWebMvc/SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+            list.Add(new Department { Id = 3, Name = "Frios" });
+            list.Add(new Department { Id = 4, Name = "Eletronics Portables" });
+
+            return View(list);
+        }
+    }
+}
