c158eb4840a626fd323e63f2d7bd9fe569c1da20
Tavilo Breno <breno_wk2@hotmail.com>
2023-05-19 02:13:07 -0300

    Adicionando tela de criação e algumas migrações

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SalesWebMvc.Models
@@ -25,7 +25,7 @@ namespace SalesWebMvc.Models
             Sellers.Add(seller);
         }
 
-        public double TotalSalles(DateTime initial, DateTime final)
+        public double TotalSales(DateTime initial, DateTime final)
         {
             return Sellers.Sum(seller => seller.TotalSales(initial, final));
         }
